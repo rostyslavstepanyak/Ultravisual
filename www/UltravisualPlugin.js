5756ef71e8f245e3bd96d3ed51a8a636fd292318
Rostyslav Stepanyak <ros.aphex@gmail.com>
2015-07-06 16:07:16 +0300

    add selection callbacks

--- www/UltravisualPlugin.js
+++ www/UltravisualPlugin.js
@@ -6,11 +6,11 @@ var UltravisualPlugin = {
 		exec(successCallback, errorCallback, 'UltravisualPlugin', 'create', []);
 	},
     createItem:  function(successCallback, errorCallback, label, details, image, options) {
-       /*var tag = UltravisualPlugin.tag++;
+       var tag = UltravisualPlugin.tag++;
        if (options && 'onSelect' in options && typeof(options['onSelect']) == 'function') {
           UltravisualPlugin.callbacks[tag] = {'onSelect':options.onSelect,'name':name};
        }
-    */
+    
        cordova.exec(successCallback,errorCallback, 'UltravisualPlugin', 'createItem', [label, details, image, options]);
     },
     onItemSelected : function(tag)
