e79256e8995c29e643be507d3f07573798d2cbbc
Rostyslav Stepanyak <ros.aphex@gmail.com>
2015-07-07 00:10:03 +0300

    add test images

--- www/UltravisualPlugin.js
+++ www/UltravisualPlugin.js
@@ -9,15 +9,12 @@ var UltravisualPlugin = {
     createItem:  function(successCallback, errorCallback, label, details, image, options) {
        var tag = UltravisualPlugin.tag++;
        if (options && 'onSelect' in options && typeof(options['onSelect']) == 'function') {
-           alert('inside');
           UltravisualPlugin.callbacks[tag] = {'onSelect':options.onSelect,'name':name};
        }
     
        cordova.exec(successCallback,errorCallback, 'UltravisualPlugin', 'createItem', [label, details, image, options]);
     },
-    onItemSelected : function(tag)
-    {
-        alert(tag);
+    onItemSelected : function(tag) {
         UltravisualPlugin.selectedItem = tag;
         if (typeof(UltravisualPlugin.callbacks[tag].onSelect) == 'function')
             UltravisualPlugin.callbacks[tag].onSelect(UltravisualPlugin.callbacks[tag].name);
